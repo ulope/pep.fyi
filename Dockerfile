fefa7cd7d6938fdd30e67053cb6f65aa7f068990
Ulrich Petri <ulo@ulo.pe>
2017-07-19 17:40:39 +0200

    Initial import

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.6
+
+MAINTAINER Ulrich Petri <mail@ulo.pe>
+
+ADD requirements.txt /requirements.txt
+
+RUN python3 -m pip install -r requirements.txt
+
+ADD app.py /app.py
+
+ENTRYPOINT ["/usr/bin/python3", "/app.py"]
---
+++ app.py
@@ -0,0 +1,26 @@
+import logging
+
+from flask import Flask, redirect
+
+log = logging.getLogger(__name__)
+
+PEP_URL_TPL = "https://www.python.org/dev/peps/pep-{:04d}/"
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>pep.fyi</h1><p>Usage: go to <b>https://pep.fyi/&lt;pep-number&gt;</b> to be redirected to the corresponding <a href='https://www.python.org/dev/peps/pep-0001/'>PEP</a></p><p>Alternatively enter the pep number below:<br><input type='text' id='number'><button id='go'>Go</button><script>document.getElementById('go').onclick=function() {window.location.href=window.location.href + document.getElementById('number').value}</script>"
+
+
+@app.route("/<int:number>")
+def pep(number):
+    log.info("PEP: %d", number)
+    return redirect(PEP_URL_TPL.format(number))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+    app.run()
